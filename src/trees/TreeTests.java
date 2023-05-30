0709d2b323bef3e6bf1f4e1190d5ddc3a6744f17
Alexander Ereshkin <xcode111@mail.ru>
2023-05-31 01:27:45 +0300

    Add check contains duplicates in BinaryTree.

---
+++ src/trees/TreeTests.java
@@ -0,0 +1,15 @@
+package trees;
+
+public class TreeTests {
+	public static void main(String[] args) throws Exception {
+		SimpleTree<Integer> tree1 = new SimpleTree<>(Integer::parseInt);
+		tree1.fromBracketNotation("8 (6 (4 (5), 6), 5 (, 5 (2, 8)))");
+		boolean result1 = tree1.checkForDuplicatesTask();
+		System.out.println(result1);
+
+		SimpleTree<Integer> tree2 = new SimpleTree<>(Integer::parseInt);
+		tree2.fromBracketNotation("11 (12 (4 (5), 6), 7 (, 8 (9, 10)))");
+		boolean result2 = tree2.checkForDuplicatesTask();
+		System.out.println(result2);
+	}
+}
